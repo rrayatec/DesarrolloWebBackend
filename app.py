0671683e22b1139cbc2fe93fc1a55cd1492eb6c6
Ruben Raya Delgado <rraya@tec.mx>
2022-04-07 20:54:21 -0500

    trabajo con estructuras de datos

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template, request
-
+import email
+from flask import Flask, redirect, render_template, request, session, url_for
+import datetime
 # FlASK
 #############################################################
 app = Flask(__name__)
+app.permanent_session_lifetime = datetime.timedelta(days=365)
+app.secret_key = "super secret key"
 #############################################################
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    email = None
+    if "email" in session:
+        email = session["email"]
+        return render_template('index.html', data=email)
+    else:
+        return render_template('Login.html', data=email)
 
 
 @app.route('/signup')
@@ -21,10 +29,14 @@ def signup():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if (request.method == "GET"):
-        return render_template("Login.html", data="email")
+    email = None    
+    if "email" in session:
+        return render_template('index.html', data=session["email"])
     else:
-        email = None
-        email = request.form["email"]
-        password = request.form["password"]
-        return render_template("index.html", data=email)
+        if (request.method == "GET"):
+            return render_template("Login.html", data="email")
+        else:
+            email = request.form["email"]
+            password = request.form["password"]
+            session["email"] = email
+            return render_template("index.html", data=email)
